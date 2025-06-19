ca4115f99d1c624d278645b8d654c318df9fd48b
LOO-kees <tlswjs16@gmail.com>
2025-06-19 10:57:50 +0900

    250619업데이트’ git log  git commit -m  250619업데이트’

---
+++ php/mobile_footer.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>모바일 푸터</title> 
+<!-- 폰트어썸cdn -->
+  <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css' rel='stylesheet' type='text/css'>
+  <!-- mo_footer.css -->
+  <link href='../css/mo_footer.css' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  
+    <!--  모바일 푸터 영역 -->
+    <div class='footer_toggle'>
+      <div>
+        <input type='checkbox' id='mo_footer_toggle'>
+        <label for="mo_footer_toggle" class='footer_label'>LotteCinema<i class="fa-solid fa-angle-down"></i></label>
+
+        <div class='footer_address'>
+          <p>서울특별시 송파구 올림픽로 295 삼성생명 잠실빌딩 18F</p>
+          <p>대표 이메일 lottecultureworks@lotte.net 고객센터 1544-8855</p>
+          <p>(유료)사업자등록번호 313-87-00979 통신판매업신고번호 제1184호</p>
+          <p>대표이사 최병환 개인정보 보호 책임자 김병문 호스팅 제공자 롯데이노베이</p>
+        </div>
+      </div>
+      <ul class='f_gnb'>
+        <li><a href="#" title='이용약관'>이용약관</a></li>
+
+        <li><a href="#" title='개인정보처리방침'>개인정보처리방침</a></li>
+
+        <li><a href="#" title='이메일무단수집거부'>이메일무단수집거부</a></li>
+
+        <li><a href="#" title='고정형 영상정보처리기기 운영 및 관리방침'>고정형 상정보처리기기 운영 및 관리방침</a></li>
+
+        <li><a href="#" title='광고/임대문의'>광고/임대문의</a></li>
+      </ul>
+      <p class='copyright'>Copyright&copy; LOTTE Cultureworks All Right Reserved.</p>
+    </div>
+</body>
+</html>
---
+++ php/mobile_header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>모바일 헤더</title>
+</head>
+<body>
+  <header>
+    <!-- 모바일 헤더 -->
+    <div>
+
+    </div>
+  </header>
+  <footer>
+  
+  </footer>
+</body>
+</html>
